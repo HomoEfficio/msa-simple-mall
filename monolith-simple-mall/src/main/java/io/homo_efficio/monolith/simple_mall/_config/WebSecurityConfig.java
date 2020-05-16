c9b5a52179f366d0f39950329213e9c4fc6e180c
Homo Efficio <homo.efficio@gmail.com>
2020-05-16 14:55:21 +0900

    Make Spring Security allow h2-console

--- monolith-simple-mall/src/main/java/io/homo_efficio/monolith/simple_mall/_config/WebSecurityConfig.java
+++ monolith-simple-mall/src/main/java/io/homo_efficio/monolith/simple_mall/_config/WebSecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.header.writers.frameoptions.WhiteListedAllowFromStrategy;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+
+import java.util.List;
 
 /**
  * @author homo.efficio@gmail.com
@@ -19,7 +23,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/actuator/health").permitAll()
                 .antMatchers(HttpMethod.POST, "/v1/sellers").permitAll()
+                .antMatchers("/h2-console/**").permitAll()
                 .anyRequest().authenticated()
+                .and()
+                .headers()
+                .addHeaderWriter(
+                        new XFrameOptionsHeaderWriter(
+                                new WhiteListedAllowFromStrategy(List.of("localhost"))    // 여기!
+                        )
+                )
+                .frameOptions().sameOrigin()
         ;
     }
 }
