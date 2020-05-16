49ae792a6c6570e988651afb314f8c44ec1a516a
Homo Efficio <homo.efficio@gmail.com>
2020-05-16 15:15:25 +0900

    Add Product controller - PUT

--- monolith-simple-mall/src/main/java/io/homo_efficio/monolith/simple_mall/controller/ProductController.java
+++ monolith-simple-mall/src/main/java/io/homo_efficio/monolith/simple_mall/controller/ProductController.java
@@ -6,10 +6,7 @@ import io.homo_efficio.monolith.simple_mall.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author homo.efficio@gmail.com
@@ -27,5 +24,11 @@ public class ProductController {
     public ResponseEntity<ProductOut> create(@RequestBody ProductIn productIn) {
         log.debug("Creating a product");
         return ResponseEntity.ok(productService.create(productIn));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductOut> update(@PathVariable("id") Long id, @RequestBody ProductIn productIn) {
+        log.debug("Updating a product");
+        return ResponseEntity.ok(productService.update(id, productIn));
     }
 }
