8744e024c1ae4436456d08b31c1ac9900fdb4e14
Homo Efficio <homo.efficio@gmail.com>
2020-05-16 14:38:01 +0900

    Add monolith module

---
+++ monolith-simple-mall/src/main/java/io/homo_efficio/monolith/simple_mall/MonolithSimpleMallApplication.java
@@ -0,0 +1,13 @@
+package io.homo_efficio.monolith.simple_mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonolithSimpleMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonolithSimpleMallApplication.class, args);
+    }
+
+}
---
+++ monolith-simple-mall/src/test/java/io/homo_efficio/monolith/simple_mall/MonolithSimpleMallApplicationTests.java
@@ -0,0 +1,13 @@
+package io.homo_efficio.monolith.simple_mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolithSimpleMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
