3670a956212bb32e2f6998f3e172fa1c9b191e7d
Homo Efficio <homo.efficio@gmail.com>
2020-05-31 14:01:24 +0900

    Add Edge Server - Spring Cloud Gateway

---
+++ formation/edge-server/src/main/java/io/homo_efficio/msa/simple_mall/formation/edge_server/EdgeServerApplication.java
@@ -0,0 +1,13 @@
+package io.homo_efficio.msa.simple_mall.formation.edge_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdgeServerApplication.class, args);
+    }
+
+}
---
+++ formation/edge-server/src/test/java/io/homo_efficio/msa/simple_mall/formation/edge_server/EdgeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.homo_efficio.msa.simple_mall.formation.edge_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
