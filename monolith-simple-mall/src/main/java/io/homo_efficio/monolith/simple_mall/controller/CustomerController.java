1e721225a08ebe8836de79622c21e315419fc554
Homo Efficio <homo.efficio@gmail.com>
2020-06-06 15:08:56 +0900

    Add Customer controller api - findByLoginId

--- monolith-simple-mall/src/main/java/io/homo_efficio/monolith/simple_mall/controller/CustomerController.java
+++ monolith-simple-mall/src/main/java/io/homo_efficio/monolith/simple_mall/controller/CustomerController.java
@@ -1,5 +1,7 @@
 package io.homo_efficio.monolith.simple_mall.controller;
 
+import io.homo_efficio.monolith.simple_mall._common.exception.EntityNotFoundException;
+import io.homo_efficio.monolith.simple_mall.domain.Customer;
 import io.homo_efficio.monolith.simple_mall.domain.repository.CustomerRepository;
 import io.homo_efficio.monolith.simple_mall.dto.CustomerIn;
 import io.homo_efficio.monolith.simple_mall.dto.CustomerOut;
@@ -7,10 +9,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author homo.efficio@gmail.com
@@ -28,5 +29,13 @@ public class CustomerController {
     @PostMapping
     public ResponseEntity<CustomerOut> create(@RequestBody CustomerIn customerIn) {
         return ResponseEntity.ok(CustomerOut.from(customerRepository.save(customerIn.toEntityWithPasswordEncoder(passwordEncoder))));
+    }
+
+    @GetMapping
+    public ResponseEntity<CustomerOut> findByLoginId(@RequestParam("loginId") String loginId) {
+        return ResponseEntity.ok(CustomerOut.from(
+                        customerRepository.findByLoginId(loginId)
+                                .orElseThrow(() -> new EntityNotFoundException(Customer.class,
+                                        String.format("판매자 [%s] 는 존재하지 않습니다.", loginId)))));
     }
 }
