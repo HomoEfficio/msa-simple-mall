495bb0fdca8271c28b492c7d7f6b9e32315e8fec
Homo Efficio <homo.efficio@gmail.com>
2020-06-06 14:57:18 +0900

    Add Customer api test - GET

--- monolith-simple-mall/src/test/java/io/homo_efficio/monolith/simple_mall/controller/CustomerControllerTest.java
+++ monolith-simple-mall/src/test/java/io/homo_efficio/monolith/simple_mall/controller/CustomerControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
@@ -20,6 +21,7 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 
 import java.util.stream.Stream;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -74,6 +76,34 @@ class CustomerControllerTest {
     }
 
     private static Stream<Arguments> customers() {
+        return Stream.of(
+                Arguments.of("열글자로된고객이름일", "aaaaaaaaaabbbbbbbbbbcccccccccc@dddddddd.eeeeeeeeee", "12345678901234567890", new Address("12345", "도로명 주소 11", "도로명 주소 12", "상세주소 1"), "aaaaaaaaaabbbbbbbbbbcccccccccc", "abcde12345abcde12345"),
+                Arguments.of("열글자로된고객이름이", "smile-customer@gmail.com", "12345678901234567890", new Address("54321", "도로명 주소 21", "도로명 주소 22", "상세주소 2"), "smile-customer", "12345abcde12345abcde")
+        );
+    }
+
+    @ParameterizedTest(name = "구매자 [{0} - {1} - {2} - {3} - {4} - {5}] 생성 후 {4} 검색")
+    @MethodSource("findingCustomers")
+    void findByLoginId(String name, String email, String phone, Address address, String loginId, String password) throws Exception {
+        postNewCustomer(name, email, phone, address, loginId, password);
+
+        mvc.perform(MockMvcRequestBuilders.get("/v1/customers?loginId=" + loginId)
+                .with(httpBasic(loginId, password))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("loginId").value(loginId))
+                .andExpect(jsonPath("name").value(name))
+                .andExpect(jsonPath("email").value(email))
+                .andExpect(jsonPath("phone").value(phone))
+                .andExpect(jsonPath("address.zipNo").value(address.getZipNo()))
+                .andExpect(jsonPath("address.roadAddrPart1").value(address.getRoadAddrPart1()))
+                .andExpect(jsonPath("address.roadAddrPart2").value(address.getRoadAddrPart2()))
+                .andExpect(jsonPath("address.addrDetail").value(address.getAddrDetail()))
+        ;
+    }
+
+    private static Stream<Arguments> findingCustomers() {
         return Stream.of(
                 Arguments.of("열글자로된고객이름일", "aaaaaaaaaabbbbbbbbbbcccccccccc@dddddddd.eeeeeeeeee", "12345678901234567890", new Address("12345", "도로명 주소 11", "도로명 주소 12", "상세주소 1"), "aaaaaaaaaabbbbbbbbbbcccccccccc", "abcde12345abcde12345"),
                 Arguments.of("열글자로된고객이름이", "smile-customer@gmail.com", "12345678901234567890", new Address("54321", "도로명 주소 21", "도로명 주소 22", "상세주소 2"), "smile-customer", "12345abcde12345abcde")
