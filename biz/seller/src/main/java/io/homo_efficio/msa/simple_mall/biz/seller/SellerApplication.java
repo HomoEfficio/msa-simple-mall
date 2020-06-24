9b1f2dc676c6974876b5db9cf1ad1c39be45b509
Homo Efficio <homo.efficio@gmail.com>
2020-06-24 20:10:02 +0900

    Add new module for seller
    - Change embedded mongo db from testImplementation to implementation

---
+++ biz/seller/src/main/java/io/homo_efficio/msa/simple_mall/biz/seller/SellerApplication.java
@@ -0,0 +1,13 @@
+package io.homo_efficio.msa.simple_mall.biz.seller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellerApplication.class, args);
+    }
+
+}
---
+++ biz/seller/src/test/java/io/homo_efficio/msa/simple_mall/biz/seller/SellerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.homo_efficio.msa.simple_mall.biz.seller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
