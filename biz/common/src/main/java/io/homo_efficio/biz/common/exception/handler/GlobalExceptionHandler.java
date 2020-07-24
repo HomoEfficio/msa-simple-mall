f2c3f1458c432fc05241bc630d445b047836280f
Homo Efficio <homo.efficio@gmail.com>
2020-07-24 20:53:28 +0900

    Change GlobalExceptionHandler with Reactive style

--- biz/common/src/main/java/io/homo_efficio/biz/common/exception/handler/GlobalExceptionHandler.java
+++ biz/common/src/main/java/io/homo_efficio/biz/common/exception/handler/GlobalExceptionHandler.java
@@ -1,15 +1,20 @@
 package io.homo_efficio.biz.common.exception.handler;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.homo_efficio.biz.common.exception.ResourceNotFoundException;
 import io.homo_efficio.biz.common.message.MessageResolver;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
-//import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 /**
  * @author homo.efficio@gmail.com
@@ -18,36 +23,66 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 @Slf4j
 @RequiredArgsConstructor
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler implements ErrorWebExceptionHandler {
 
     private final MessageResolver messageResolver;
+    private final ObjectMapper objectMapper;
 
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException e) {
+    @Override
+    public Mono<Void> handle(ServerWebExchange serverWebExchange, Throwable throwable) {
+        DataBufferFactory bufferFactory = serverWebExchange.getResponse().bufferFactory();
+
+        if (throwable instanceof ResourceNotFoundException) {
+            return getErrorResponseMono(serverWebExchange, bufferFactory,
+                    getErrorResponse(serverWebExchange, (ResourceNotFoundException)throwable));
+        }
+        if (throwable instanceof IllegalArgumentException) {
+            return getErrorResponseMono(serverWebExchange, bufferFactory,
+                    getErrorResponse(serverWebExchange, (IllegalArgumentException)throwable));
+        }
+
+        serverWebExchange.getResponse().setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
+        serverWebExchange.getResponse().getHeaders().setContentType(MediaType.APPLICATION_JSON);
+        return serverWebExchange.getResponse()
+                .writeWith(Mono.just(bufferFactory.wrap("Unknown error".getBytes())));
+    }
+
+    private ErrorResponse getErrorResponse(ServerWebExchange serverWebExchange, ResourceNotFoundException e) {
         log.error("handleResourceNotFoundException: {}", e.getMessage());
         ErrorResponse errorResponse;
         if (e.getResourceId() == null) {
             errorResponse = ErrorResponse.of(ErrorCode.ENTITY_NOT_FOUND_WITH_NON_ID,
                     messageResolver.getMessage(ErrorCode.ENTITY_NOT_FOUND_WITH_NON_ID.getErrorCode(), e.getMessage()),
-                    "RequestURL NOT IMPLEMENTED");
+                    serverWebExchange.getRequest().getURI().toString());
         } else {
             errorResponse = ErrorResponse.of(ErrorCode.ENTITY_NOT_FOUND,
                     messageResolver.getMessage(ErrorCode.ENTITY_NOT_FOUND.getErrorCode(),
                             e.getResourceClassSimpleName(), e.getResourceId()),
-                    "RequestURL NOT IMPLEMENTED");
-//                    ServletUriComponentsBuilder.fromCurrentRequestUri().toUriString());
+                    serverWebExchange.getRequest().getURI().toString());
         }
-        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+        return errorResponse;
     }
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+    private ErrorResponse getErrorResponse(ServerWebExchange serverWebExchange, IllegalArgumentException e) {
         log.error("handleIllegalArgumentException: {}", e.getMessage());
-        ErrorResponse errorResponse;
-        errorResponse = ErrorResponse.of(ErrorCode.ILLEGAL_ARGUMENT,
+        return ErrorResponse.of(ErrorCode.ILLEGAL_ARGUMENT,
                 messageResolver.getMessage(ErrorCode.ILLEGAL_ARGUMENT.getErrorCode(), e.getMessage()),
-                "RequestURL NOT IMPLEMENTED");
-//                    ServletUriComponentsBuilder.fromCurrentRequestUri().toUriString());
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+                serverWebExchange.getRequest().getURI().toString());
+    }
+
+    private Mono<Void> getErrorResponseMono(ServerWebExchange serverWebExchange, DataBufferFactory bufferFactory, ErrorResponse errorResponse) {
+        DataBuffer dataBuffer;
+        try {
+            dataBuffer = bufferFactory.wrap(objectMapper.writeValueAsBytes(errorResponse));
+        } catch (JsonProcessingException jsonProcessingException) {
+            jsonProcessingException.printStackTrace();
+            dataBuffer = bufferFactory.wrap("JSON processing error".getBytes());
+        }
+        serverWebExchange.getResponse()
+                .setRawStatusCode(errorResponse.getStatusCode());
+        serverWebExchange.getResponse()
+                .getHeaders().setContentType(MediaType.APPLICATION_JSON);
+        return serverWebExchange.getResponse()
+                .writeWith(Mono.just(dataBuffer));
     }
 }
