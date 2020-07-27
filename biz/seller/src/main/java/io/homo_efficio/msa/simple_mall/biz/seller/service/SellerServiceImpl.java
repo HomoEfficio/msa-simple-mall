bcc23b0e0df896e6d712f5bbb75a4f4f3a8abc89
Homo Efficio <homo.efficio@gmail.com>
2020-07-28 07:07:43 +0900

    Add logging showing thread name

--- biz/seller/src/main/java/io/homo_efficio/msa/simple_mall/biz/seller/service/SellerServiceImpl.java
+++ biz/seller/src/main/java/io/homo_efficio/msa/simple_mall/biz/seller/service/SellerServiceImpl.java
@@ -5,6 +5,7 @@ import io.homo_efficio.msa.simple_mall.biz.seller.domain.model.Seller;
 import io.homo_efficio.msa.simple_mall.biz.seller.dto.SellerIn;
 import io.homo_efficio.msa.simple_mall.biz.seller.dto.SellerOut;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,7 @@ import java.util.function.Function;
  */
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class SellerServiceImpl implements SellerService {
 
     private final ReactiveMongoTemplate template;
@@ -34,15 +36,22 @@ public class SellerServiceImpl implements SellerService {
 
     @Override
     public Mono<SellerOut> update(String sellerId, Mono<SellerIn> sellerIn) {
+        log.info(Thread.currentThread().getName() + " in update() - 1");
         Mono<Seller> sellerMono = template.findById(sellerId, Seller.class)
-                .switchIfEmpty(Mono.error(() -> new ResourceNotFoundException(Seller.class, sellerId)))
+//                .switchIfEmpty(Mono.error(() -> new ResourceNotFoundException(Seller.class, sellerId)))
+                .switchIfEmpty(Mono.error(() -> {
+                    log.info(Thread.currentThread().getName() + " in update() - 2");
+                    return new ResourceNotFoundException(Seller.class, sellerId);
+                }))
                 .flatMap(updateEntityWith(sellerIn));
 
         return SellerOut.from(template.save(sellerMono));
     }
 
     private Function<Seller, Mono<? extends Seller>> updateEntityWith(Mono<SellerIn> sellerIn) {
+        log.info(Thread.currentThread().getName() + " in updateEntityWith() - 3");
         return seller -> sellerIn.map(s -> {
+            log.info(Thread.currentThread().getName() + " in updateEntityWith() - 4");
             s.updateEntityWithPasswordEncoder(seller, passwordEncoder);
             return seller;
         });
