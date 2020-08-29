935d92dab653a2dcb62015bbda44545abf7af804
Homo Efficio <homo.efficio@gmail.com>
2020-08-29 18:08:21 +0900

    Initial commit [gh-1]

---
+++ biz/customer/src/test/java/io/homo_efficio/msa/simple_mall/biz/customer/CustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.homo_efficio.msa.simple_mall.biz.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
